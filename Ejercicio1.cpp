d7f400aa8a20555433df771a3e1cdd7a61867b2a
CodeGunner100a <codegunner178@gmail.com>
2026-09-19 12:17:02 -0400

    Update Ejercicio1.cpp

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -1,32 +1,40 @@
 // Copiar el ejercicio 1 debajo del comentario "{ EJERCICIO 1 COPIAR ABAJO }"
 
-void Eliminar(String &x, String z, byte p, byte &pos) {
-	if (p <= x.Length()) // caso general
+// EJERCICIO 1
+// 1) Función que devuelva la posición de la última
+// ocurrencia de un carácter z en una cadena x.
+//
+// Ej:
+// x = "hola laLAs", z = 'a'  => 7
+
+byte UltimaOcurrencia(String x, String z, byte p) {
+	byte pos;
+
+	if (p > x.Length()) // caso base
 	{
-		if (x[p] == z[1]) {
+		pos = 0;
+	}
+	else // caso general
+	{
+		pos = UltimaOcurrencia(x, z, p + 1);
+
+		if (x[p] == z[1] && pos == 0) {
 			pos = p;
 		}
+	}
 
-		Eliminar(x, z, p + 1, pos);
-	}
-	else // caso base
-	{
-		if (pos > 0) {
-			x.Delete(pos, 1);
-		}
-	}
+	return pos;
 }
 
 
 //==========================================================================================
 // Copiar la llamada del ejercicio 1 debajo del comentario "{ LLAMADA DEL EJERCICIO 1 COPIAR ABAJO}"
 
+
 	String x = Edit1->Text;
 	String z = Edit2->Text;
 
-	byte pos = 0; 
+	byte pos = UltimaOcurrencia(x, z, 1);
 
-	Eliminar(x, z, 1, pos);
-
-	Edit3->Text = x;
+	Edit3->Text = pos;
 
