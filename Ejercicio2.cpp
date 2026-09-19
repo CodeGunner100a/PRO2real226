04ea384ae65f67b683e8d0a7b525081e1cf9f346
CodeGunner100a <codegunner178@gmail.com>
2026-09-19 09:38:10 -0400

    Update Ejercicio2.cpp

--- Ejercicio2.cpp
+++ Ejercicio2.cpp
@@ -1,6 +1,6 @@
 // Copiar el ejercicio 2 debajo del comentario "{ EJERCICIO 2 COPIAR ABAJO }"
 
-byte ContPrim(String x) {
+byte ContVocal(String x) {
 	byte c;
 
 	if (x == "") // caso base
@@ -12,9 +12,9 @@ byte ContPrim(String x) {
 		wchar_t z = x[1];
 		x.Delete(1, 1);
 
-		c = ContPrim(x);
+		c = ContVocal(x);
 
-		if (z == '2' || z == '3' || z == '5' || z == '7') {
+		if (z == 'a' || z == 'e' || z == 'i' || z == 'o' || z == 'u') {
 			c++;
 		}
 	}
@@ -22,30 +22,32 @@ byte ContPrim(String x) {
 	return c;
 }
 
-void CargarDigVec(TStringGrid *v, String x, byte a, byte b) {
+void CargarVocal(TStringGrid *v, String x, byte a, byte b) {
 	byte n = b - a + 1;
 
 	if (n > 0) {
-		if (x[1] == '2' || x[1] == '3' || x[1] == '5' || x[1] == '7') {
+		if (x[1] == 'a' || x[1] == 'e' || x[1] == 'i' || x[1] == 'o' ||
+			x[1] == 'u') {
+
 			v->Cells[a][0] = x[1];
 			a++;
 		}
 
 		x.Delete(1, 1);
-		CargarDigVec(v, x, a, b);
+		CargarVocal(v, x, a, b);
 	}
 }
+
 
 //==========================================================================================
 // Copiar la llamada del ejercicio 2 debajo del comentario "{ LLAMADA DEL EJERCICIO 2 COPIAR ABAJO}"
 	String x = Edit1->Text;
 
-	byte n = ContPrim(x);
+	byte n = ContVocal(x);
 
-	CargarDigVec(StringGrid1, x, 0, n - 1);
+	CargarVocal(StringGrid1, x, 0, n - 1);
 
 	StringGrid1->ColCount = n;
 	StringGrid1->RowCount = 1;
 
 	Edit3->Text = n;
-
